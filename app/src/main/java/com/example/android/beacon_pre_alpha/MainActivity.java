0a76affc9690ce04e22e42927a6be07efc2f0733
DESKTOP-6SIUNN9\James <James01lw1998@gmail.com>
2018-11-12 14:22:01 +1300

    Added API Key and basic implementations

--- app/src/main/java/com/example/android/beacon_pre_alpha/MainActivity.java
+++ app/src/main/java/com/example/android/beacon_pre_alpha/MainActivity.java
@@ -1,13 +1,83 @@
 package com.example.android.beacon_pre_alpha;
 
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.RelativeLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.nearby.messages.Message;
+import com.google.android.gms.nearby.messages.MessageListener;
+import com.google.android.gms.nearby.messages.Messages;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        SharedPreferences.OnSharedPreferenceChangeListener{
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static final int PERMISSIONS_REQUEST_CODE = 1111;
+
+    private static final String KEY_SUBSCRIBED = "subscribed";
+
+    /**
+     * The entry point to Google Play Services.
+     */
+    private GoogleApiClient mGoogleApiClient;
+
+    /**
+     * The container {@link android.view.ViewGroup} for the minimal UI associated with this sample.
+     */
+    private RelativeLayout mContainer;
+
+    /**
+     * Tracks subscription state. Set to true when a call to
+     * {@link Messages#subscribe(GoogleApiClient, MessageListener)} succeeds.
+     */
+    private boolean mSubscribed = false;
+
+    /**
+     * Adapter for working with messages from nearby beacons.
+     */
+    private ArrayAdapter<String> mNearbyMessagesArrayAdapter;
+
+    /**
+     * Backing data structure for {@code mNearbyMessagesArrayAdapter}.
+     */
+    private List<String> mNearbyMessagesList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+
+    }
+
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
